b262abd0d468751df3a97d190bbd584dd9407f0b
jixjia <>
2020-08-26 06:16:28 +0000

    Added graph

--- Hello World.py
+++ Hello World.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC ## Hello World
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC <img src='https://jixjiastorage.blob.core.windows.net/public/images/sais.png' width='80%'>
